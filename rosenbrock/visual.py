e6efacd1e24fdfe4e34927eaeba75e701b41ae89
leobvital <leobvital@gmail.com>
2017-12-30 13:29:33 -0200

    mudança nos gráficos e no histograma

--- rosenbrock/visual.py
+++ rosenbrock/visual.py
@@ -7,8 +7,10 @@ Created on Sat Feb 21 03:35:33 2015
 
 #histogram equalization of the data for viazualization
 def histeq(data):
-    from numpy import histogram, interp, max, size, sqrt
-    nbins=int(sqrt(size(data)))   
+    from numpy import histogram, interp, max, size, sqrt, min
+    nbins=int(max(data) - min(data))
+    if nbins == 0:
+        nbins += 1
     # Histogram
     hist,bins = histogram(data.flatten(),nbins,normed=True)
     cdf = hist.cumsum() # Cumulative distribution function
